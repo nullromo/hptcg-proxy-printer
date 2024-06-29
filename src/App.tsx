44c46347840526f71301e5a2da1468c8207f5bfa
Kyle Kovacs <kylerobertkovacs@gmail.com>
2024-06-29 13:59:46 -0700

    add some notes. Stuck for now

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,13 @@ const App = () => {
                 onClick={() => {
                     const form = new FormData();
                     form.append('searchText', name);
+                    form.append('sortBy', 'sn');
+                    form.append('sortOrder', 'asc');
+
+                    // TODO: I need to get around the CORS issue, but I'm not sure how to do it.
+
+                    // TODO: I tried curl -v -F searchText=lesson -F sortBy=sn -F sortOrder=asc -X POST https://accio.cards/search?handler=SearchCards and I just got 400 Bad Request. Not sure what's different about this request vs. the browser's request
+
                     axios
                         .post(
                             'https://accio.cards/search?handler=SearchCards',
